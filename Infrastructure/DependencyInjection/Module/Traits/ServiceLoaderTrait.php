0303d80e4103e7aa25bea583a441100340ffaf1d
Sebastiaan Stok <s.stok@rollerscapes.net>
2019-02-02 14:58:04 +0100

    fix(Core): Wrong loader in ServiceLoaderTrait
    The Dependency Injection component has it’s own GlobFileLoader

--- Infrastructure/DependencyInjection/Module/Traits/ServiceLoaderTrait.php
+++ Infrastructure/DependencyInjection/Module/Traits/ServiceLoaderTrait.php
@@ -16,11 +16,11 @@ namespace ParkManager\Module\CoreModule\Infrastructure\DependencyInjection\Modul
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Config\Loader\DelegatingLoader;
-use Symfony\Component\Config\Loader\GlobFileLoader;
 use Symfony\Component\Config\Loader\LoaderResolver;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\ClosureLoader;
 use Symfony\Component\DependencyInjection\Loader\DirectoryLoader;
+use Symfony\Component\DependencyInjection\Loader\GlobFileLoader;
 use Symfony\Component\DependencyInjection\Loader\IniFileLoader;
 use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
@@ -36,7 +36,7 @@ trait ServiceLoaderTrait
             new XmlFileLoader($container, $locator),
             new YamlFileLoader($container, $locator),
             new IniFileLoader($container, $locator),
-            new GlobFileLoader($locator),
+            new GlobFileLoader($container, $locator),
             new DirectoryLoader($container, $locator),
             new ClosureLoader($container),
         ]);
